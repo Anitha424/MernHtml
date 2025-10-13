25f74c7991d19ae55af60905ebe881dbb903d83f
Anitha424 <baskaranitha2005@gmail.com>
2025-10-13 08:38:47 +0530

    first commit

---
+++ scr1.js
@@ -0,0 +1,24 @@
+console.log("yt");
+let obj1={age:33};
+let obj2=obj1;
+obj2.age=44;
+console.log(obj1.age);
+
+logical operators
+let a=true;
+let b=false;
+if(a||b)
+console.log("hello");
+else
+console.log("bye");
+
+functions
+function sum(a,b){
+    console.log(a+b);
+}
+const sum2=(a,b,c)=>{
+    console.log(a+b+c);
+}
+sum2(11,22,33);
+sum2(100,200,300);
+
